749dccd569cc7fba69ac45ffd4810e0066c2287a
altinawiwaseem <wasemm@live.com>
2023-05-12 20:43:34 +0200

    adding routes for the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import { Button } from "react-bootstrap";
 import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import AddProduct from "./components/AddProduct";
+import UpdateProduct from "./components/UpdateProduct";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <h1>E-Commerce project</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/add" element={<AddProduct />} />
+
+        <Route path="/update" element={<UpdateProduct />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">E-COMMERCE</Navbar.Brand>
+          <Nav className="mr-auto navbar-wrapper">
+            <Link to="/add">Add Product</Link>
+            <Link to="/update">Update Products</Link>
+            <Link to="login">Login</Link>
+            <Link to="register">Register</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
