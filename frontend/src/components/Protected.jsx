b3cb22a5a64f242d7a05844cb28cfcb4212c8c50
altinawiwaseem <wasemm@live.com>
2023-05-14 20:18:07 +0200

    w

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 const Protected = (props) => {
   const Cmp = props.Cmp;
+
+  const navigate = useNavigate();
+  const { userAuth } = useContext(UserContext);
+  useEffect(() => {
+    if (userAuth) {
+      navigate("/add");
+    } else {
+      navigate("/register");
+    }
+  }, []);
 
   return (
     <div>
