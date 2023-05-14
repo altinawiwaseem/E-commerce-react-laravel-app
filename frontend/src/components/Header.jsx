1d79c216c431f0ce943c22eec0433d5b5c206583
altinawiwaseem <wasemm@live.com>
2023-05-14 21:07:11 +0200

    logout function

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
 
 const Header = () => {
-  const { userAuth } = useContext(UserContext);
+  const { userAuth, handleLogout } = useContext(UserContext);
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">E-COMMERCE</Navbar.Brand>
-          <Nav className="mr-auto navbar-wrapper">
+          <Nav className="me-auto navbar-wrapper">
             {userAuth ? (
               <>
                 <Link to="/add">Add Product</Link>
@@ -24,6 +24,15 @@ const Header = () => {
               </>
             )}
           </Nav>
+          {userAuth && (
+            <Nav>
+              <NavDropdown title="User Name">
+                <NavDropdown.Item onClick={handleLogout}>
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          )}
         </Container>
       </Navbar>
     </div>
--- frontend/src/components/UserContext.jsx
+++ frontend/src/components/UserContext.jsx
@@ -1,9 +1,12 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserContext = createContext(null);
 
 const UserContextProvider = ({ children }) => {
   const [userAuth, setUserAuth] = useState(localStorage.getItem("user"));
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (localStorage.getItem("user")) {
@@ -11,8 +14,14 @@ const UserContextProvider = ({ children }) => {
     }
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUserAuth(false);
+    navigate("/login");
+  };
+
   return (
-    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+    <UserContext.Provider value={{ userAuth, setUserAuth, handleLogout }}>
       {children}
     </UserContext.Provider>
   );
